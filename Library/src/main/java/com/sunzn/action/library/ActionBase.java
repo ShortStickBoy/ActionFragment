8342a1666691de23b4ed96041efa3d6072b14543
sunzn <sunzn.cn@gmail.com>
2018-06-07 15:09:45 +0800

    Update ActionBase

--- Library/src/main/java/com/sunzn/action/library/ActionBase.java
+++ Library/src/main/java/com/sunzn/action/library/ActionBase.java
@@ -5,6 +5,10 @@ import android.support.v4.app.DialogFragment;
 
 public abstract class ActionBase extends DialogFragment {
 
+    public interface DismissListener {
+        void onDismiss();
+    }
+
     public abstract int getLayoutRes();
 
 }
